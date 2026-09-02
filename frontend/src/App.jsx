31709f0b370bd15cf6602f7ccbd6060f5f2356d8
kprakash18 <iamkprakash18@gmail.com>
2026-09-02 16:28:01 +0530

    create first time usr onboarding screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import LoginPage from '@/features/auth/pages/LoginPage';
 import ActivateAccountPage from './features/auth/pages/ActivateAccountPage';
+import AcceptInvitationPage from './features/invitation/pages/AcceptInvitationPage';
 
 export default function App() {
   return(
     <>
       <LoginPage />
       <ActivateAccountPage />
+      <AcceptInvitationPage />
     </>
   );
   
